a4da49b7bf438d0927e0b25c9160a3d86ac43982
luisalbeto <luis-alberto11@hotmail.com>
2024-01-13 18:18:39 -0400

    Finishing Marketing Page

--- app/(maketing)/layout.tsx
+++ app/(maketing)/layout.tsx
@@ -1,3 +1,6 @@
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
+
 const MarketingLayout = ({
   children,
 }:{
@@ -5,13 +8,13 @@ const MarketingLayout = ({
 }) => {
   return(
     <div className="h-full bg-slate-100">
-      {/*Navbar*/}
+      <Navbar/>
      <main 
       className="pt-40 pb-20 bg-slate-100
         ">
        {children}
      </main>
-     {/*Footer*/}
+     <Footer/>
     </div>
   )
 }
--- app/(maketing)/page.tsx
+++ app/(maketing)/page.tsx
@@ -1,4 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+
 import { Medal } from "lucide-react";
+
+import Link from "next/link";
+
+import localFont from "next/font/local";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2"
+});
 
 const MarketingPage = () => {
   return(
@@ -6,14 +18,29 @@ const MarketingPage = () => {
     <div
       className="flex items-center justify-center flex-col
     ">
-      <div className="flex items-center justify-center flex-col">
+      <div className={cn("flex items-center justify-center flex-col",
+        headingFont.className   
+      )}>
         <div className="mb-4 flex items-center border shadow-sm p-4
-        bg-indigo-400 text-indigo-600 rounded-full uppercase">
+        bg-cyan-400 text-blue-600 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2"/>
           No 1 Task managment
         </div>
-
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 ">
+          Taskify helps Team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          work forward.
+        </div>
       </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">
+          Get Taskify for Free
+        </Link>
+      </Button>
     </div>
   )
 }
